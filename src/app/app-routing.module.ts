303b25241ee49f64dc763a727d97f8e43693f6a9
s0575347 <johannes.plath@student.htw-berlin.de>
2021-11-30 12:17:11 +0100

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from "./app.component";
 import {LoginComponent} from "./login/login.component";
 import {MainLayoutComponent} from "./main-layout/main-layout.component";
+import {AdminSelectPageComponent} from "./admin-select-page/admin-select-page.component";
 
 const routes: Routes = [{
   path: '', component: MainLayoutComponent, children: [
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'admin-select', component: AdminSelectPageComponent},
   ]}
 ];
 
